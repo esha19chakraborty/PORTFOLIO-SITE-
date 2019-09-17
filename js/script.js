528a7ea228f3d4d0bcb86a263466b215084f1e6c
Esha Chakraborty <esha.c.chakraborty@gmail.com>
2019-09-16 18:00:42 -0700

    changed visited links color to purple

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-console.log ("Hi there! Thanks for stopping by.")
+console.log('Hi there! Thanks for stopping by.')
 
-function menuToggle() {
-var x = document.getElementById('myNavtoggle');
-if (x.className === 'navtoggle') {
-  x.className += ' responsive';}
-  else {
-    x.className = 'navtoggle';
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
   }
 }
